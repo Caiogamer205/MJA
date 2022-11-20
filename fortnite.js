8709590303975b0cd456a469c984aaf3dd7450f8
Caiogamer205 <117364086+Caiogamer205@users.noreply.github.com>
2022-11-20 00:13:51 -0300

    Add files via upload

---
+++ fortnite.js
@@ -0,0 +1,78 @@
+function mapas1(){
+    var mapas = (document.getElementById("mapas1").value);
+    if(mapas == "Capítulo 1 Temporada 1"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap1Season1.jpg' width = '750' height = '750'>";
+        document.getElementById("video").innerHTML = "<iframe width='535' height='300' src='https://www.youtube.com/embed/MA_G2DveC_M' title='Evolution of Fortnite Map  (Chapter 1 Season 1 - Chapter 3 Season 4)' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    }
+    else if(mapas == "Capítulo 1 Temporada 2"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap1Season2.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 1 Temporada 3"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap1Season3.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 1 Temporada 4"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap1Season4.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 1 Temporada 5"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap1Season5.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 1 Temporada 6"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap1Season6.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 1 Temporada 7"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap1Season7.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 1 Temporada 8"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap1Season8.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 1 Temporada 9"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap1Season9.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 1 Temporada X"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap1SeasonX.jpg' width = '750' height = '750'>";
+    }
+}
+
+function mapas2(){
+    var mapas = (document.getElementById("mapas2").value);
+    if(mapas == "Capítulo 2 Temporada 1"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap2Season1.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 2 Temporada 2"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap2Season2.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 2 Temporada 3"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap2Season3.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 2 Temporada 4"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap2Season4.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 2 Temporada 5"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap2Season5.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 2 Temporada 6"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap2Season6.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 2 Temporada 7"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap2Season7.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 2 Temporada 8"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap2Season8.jpg' width = '750' height = '750'>";
+    }
+}
+
+function mapas3(){
+    var mapas = (document.getElementById("mapas3").value);
+    if(mapas == "Capítulo 3 Temporada 1"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap3Season1.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 3 Temporada 2"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap3Season2.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 3 Temporada 3"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap3Season3.jpg' width = '750' height = '750'>";
+    }
+    else if(mapas == "Capítulo 3 Temporada 4"){
+        document.getElementById("img").innerHTML = "<img src = 'IMG Mapas Fortnite/Cap3Season4.jpg' width = '750' height = '750'>";
+    }
+}
