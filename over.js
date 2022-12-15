7712e1b86b700126c317cd737a7fa5e2e6dc4cfd
Caiogamer205 <117364086+Caiogamer205@users.noreply.github.com>
2022-12-14 21:40:37 -0300

    Add files via upload

---
+++ over.js
@@ -0,0 +1,30 @@
+function toggle(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active')
+}
+function toggle2(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup2 = document.getElementById('popup2');
+    popup2.classList.toggle('active')
+}
+function toggle3(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup3 = document.getElementById('popup3');
+    popup3.classList.toggle('active')
+}
+function toggle4(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup4 = document.getElementById('popup4');
+    popup4.classList.toggle('active')
+}
+function toggle5(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup5 = document.getElementById('popup5');
+    popup5.classList.toggle('active')
+}
